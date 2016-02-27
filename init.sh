1932966ba4acfd8fc2eb18d0a616df6bb7264e1b
wujinyu <jinyu.wu@archermind.com>
2016-02-27 15:17:08 +0800

    aaaa

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "# HWZD-2016H1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/wujinyu/HWZD-2016H1.git
+git push -u origin master
+
+
