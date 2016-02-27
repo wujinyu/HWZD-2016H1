21dc0c1201a54ffe7bf4f22daf27a4291d0dc5fa
wujinyu <jinyu.wu@archermind.com>
2016-02-27 16:06:25 +0800

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-echo "# HWZD-2016H1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/wujinyu/HWZD-2016H1.git
-git push -u origin master
-
-
